2379a43984ab25d8578011a8eebfb2780c92f75b
Szymon Kozłowski <szymonkozlowski48@gmail.com>
2023-06-02 20:21:00 +0200

    udostępnianie czy coś

--- app/src/main/java/com/example/cocktails/ActionActivity.java
+++ app/src/main/java/com/example/cocktails/ActionActivity.java
@@ -1,17 +1,17 @@
 package com.example.cocktails;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
-
 public class ActionActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_action);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/cocktails/MainActivity.java
+++ app/src/main/java/com/example/cocktails/MainActivity.java
@@ -3,19 +3,25 @@ package com.example.cocktails;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.FragmentTransaction;
 import com.example.cocktails.ui.main.MainFragment;
 import android.content.Intent;
 import android.view.MenuItem;
 import android.view.Menu;
 public class MainActivity extends AppCompatActivity implements CocktailListFragment.Listener{
-
+    private ShareActionProvider shareActionProvider;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Przygotowanie menu i dodanie elementów do paska aplikacji
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Blablablablablla");
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -23,7 +29,7 @@ public class MainActivity extends AppCompatActivity implements CocktailListFragm
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.toolbar: //IDK if this is correct id but im going to sleep lol
-// Kod wykonywany po kliknięciu elementu Action_Activity
+                // Kod wykonywany po kliknięciu elementu Action_Activity
                 Intent intent = new Intent(this, ActionActivity.class);
                 startActivity(intent);
                 return true;
@@ -56,5 +62,14 @@ public class MainActivity extends AppCompatActivity implements CocktailListFragm
             intent.putExtra(DetailActivity.EXTRA_COCKTAIL_ID, (int) id);
             startActivity(intent);
         }
+    }
+
+
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        shareActionProvider.setShareIntent(intent);
     }
 }
